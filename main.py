85f4a84f02d3754f506e216d66949f454b387f9e
nponticiello <72758640+nponticiello@users.noreply.github.com>
2022-09-14 22:06:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from src import sourcecolumns
 from src import sourcedata
 
 def columns_extractions():
-    rel_path = 'data/source/SOURCECOLUMNS.txt'
-    relfilepath = sourcecolumns.ColumnExtraction.filepath(rel_path)
+    file_column_name = 'SOURCECOLUMNS.txt'
+    relfilepath = sourcecolumns.ColumnExtraction.filepath(file_column_name)
     headerslist = sourcecolumns.ColumnExtraction.obtaining_headers(relfilepath)
     sourcecolumns.ColumnExtraction.uploadingheaders(headerslist)
 
 def data_extraction():
-    rel_path = 'data/source/SOURCEDATA.txt'
-    filepath = sourcedata.DataExtraction.obtainingfilepath(rel_path)
+    file_source_name = 'SOURCEDATA.txt'
+    filepath = sourcedata.DataExtraction.obtainingfilepath(file_source_name)
     sourcedata.DataExtraction.addingdata(filepath)
 
 
