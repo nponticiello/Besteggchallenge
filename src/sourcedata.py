280b9e803745fbb8f131f3898acf91ca4c80c91c
nponticiello <72758640+nponticiello@users.noreply.github.com>
2022-09-15 08:41:44 -0400

    Update sourcedata.py

--- src/sourcedata.py
+++ src/sourcedata.py
@@ -1,16 +1,10 @@
 import csv
 import os
 from src import some_storage_library
-#Used a similar method as the sourcecolumns.py to obtianing the file. Then was able to add the data to the file by iterating through using .split
+#Going to iterate over the file being receieved then as that is occuring we are going to open the output.csv and split the line, so we can write the row
 #Note that with open has an 'a' because we are appending not overwritting this allows us to keep the header row
 
 class DataExtraction:
-
-    def obtainingfilepath(rel_path:str)->str:
-        path_file =  'data/source/' + rel_path
-        dir = os.path.dirname(__file__)
-        PATH_NAME = os.path.join(os.path.split(dir)[0], path_file)
-        return PATH_NAME
 
     def addingdata(path_name:str):
         file = open(path_name, "r")
